0557d8299c994b40b2b8158e769dddf03e7e0505
gunny6026 <gunny6026@naver.com>
2022-05-07 13:04:51 +0900

    feat : BOJ 11660 구간 합 구하기 5

---
+++ solved/silver1/11660.cpp
@@ -0,0 +1,26 @@
+﻿#include "bits/stdc++.h"
+
+#define MX 1025
+
+using namespace std;
+
+int arr[MX][MX];
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            cin >> arr[i][j];
+            arr[i][j] += arr[i - 1][j] + arr[i][j - 1] - arr[i - 1][j - 1];
+        }
+    }
+    for (int i = 0; i < m; ++i) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        cout << arr[x2][y2] - arr[x1 - 1][y2] - arr[x2][y1 - 1] + arr[x1 - 1][y1 - 1] << '\n';
+    }
+}
