2ea3135ad8d9b7afd09cd843358cf8132de25d34
gunny6026 <gunny6026@naver.com>
2022-05-17 17:15:32 +0900

    feat : boj 11000 강의실 배정

---
+++ study/2022/week19/11000.cpp
@@ -0,0 +1,36 @@
+﻿#include "bits/stdc++.h"
+
+using namespace std;
+
+bool comp(pair<int, int> &a, pair<int, int> &b) {
+    if (a.first == b.first) {
+        return a.second < b.second;
+    }
+    return a.first < b.first;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n;
+    cin >> n;
+    vector<pair<int, int>> v;
+    for (int i = 0; i < n; ++i) {
+        int s, e;
+        cin >> s >> e;
+        v.push_back({s, e});
+    }
+    sort(v.begin(), v.end(), comp);
+    priority_queue<int, vector<int>, greater<int>> pq;
+    pq.push(v[0].second);
+    int len = v.size();
+    for (int i = 1; i < len; i++) {
+        int prev = pq.top();
+        if (prev <= v[i].first) {
+            pq.pop();
+        }
+        pq.push(v[i].second);
+    }
+    cout << pq.size();
+}
---
+++ study/2022/week19/1490.cpp
@@ -0,0 +1,40 @@
+﻿#include "bits/stdc++.h"
+
+using namespace std;
+typedef long long ll;
+
+bool d[10];
+
+void init(ll num) {
+    while (num) {
+        d[num % 10] = true;
+        num /= 10;
+    }
+}
+
+bool solve(ll num) {
+    for (int i = 1; i <= 9; ++i) {
+        if (!d[i]) continue;
+        if (num % i) return false;
+    }
+    return true;
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll answer , loop = 1;
+    cin >> answer;
+    init(answer);
+    while (1) {
+        for(int i=0; i < loop; i++){
+            if(solve(answer*loop+i)){
+                cout << answer*loop+i;
+                exit(0);
+            }
+        }
+        loop *= 10;
+    }
+}
