90b675d11c65088f142329976a1043872ab7d8ce
gunny6026 <gunny6026@naver.com>
2022-05-31 15:45:40 +0900

    feat : BOJ 16943 숫자 재배치

---
+++ study/2022/week21/16943.cpp
@@ -0,0 +1,20 @@
+﻿#include "bits/stdc++.h"
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    string a;
+    int b;
+    cin >> a >> b;
+    sort(a.begin(),a.end());
+    int ans = -1;
+    do {
+        if(a[0] == '0' || stoi(a) >= b) continue;
+        ans = max(ans, stoi(a));
+
+    } while (next_permutation(a.begin(),a.end()));
+    cout << ans;
+}
