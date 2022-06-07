4324e71d2a9135cd3b6a7971e6c181753b0e028a
gunny6026 <gunny6026@naver.com>
2022-06-07 23:57:29 +0900

    feat : BOJ 1388 바닥 장식

--- solved/silver1/1495.cpp
+++ solved/silver1/1495.cpp
@@ -4,38 +4,25 @@ using namespace std;
 
 int n, s, m, ans = -1;
 int arr[52];
-int dp[51][2];
+bool dp[51][1001];
 
-void dfs(int volume, int L) {
-    if (L == n) {
-        ans = max(ans, volume);
-        return;
-    }
-
-    if (volume + arr[L] <= m) {
-        dfs(volume + arr[L], L + 1);
-    }
-    if (volume - arr[L] >= 0) {
-        dfs(volume - arr[L], L + 1);
-    }
-}
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     cin >> n >> s >> m;
-    for (int i = 0; i < n; ++i) {
+    for (int i = 1; i <= n; ++i) {
         cin >> arr[i];
     }
-    dp[0][0] = dp[0][1] = s;
+    dp[0][s] = 1;
     for (int i = 1; i <= n; ++i) {
-        dp[i][0] = dp[i-1][0]+arr[i-1];
-        dp[i][0] = dp[i-1][1]+arr[i-1];
-        dp[i][1] = dp[i-1][0]-arr[i-1];
-        dp[i][1] = dp[i-1][1]-arr[i-1];
+        for (int j = 0; j <= m; ++j) {
+            if(dp[i-1][j]){
+
+            }
+        }
     }
-    dfs(s, 0);
     ans == -1 ? cout << -1 : cout << ans;
 
 }
---
+++ study/2022/week22/5545.cpp
@@ -0,0 +1,29 @@
+﻿#include "bits/stdc++.h"
+
+using namespace std;
+
+int arr[101];
+
+bool cmp(int a, int b) {
+    return a > b;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n, a, b, c;
+    cin >> n >> a >> b >> c;
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + n, cmp);
+    int sum = 0, ans = 0;
+    for (int i = 0; i < n; ++i) {
+        sum += arr[i];
+        int tmp = (sum + c) / (a + (i + 1) * b);
+        ans = max(tmp, ans);
+    }
+    ans = max(ans,c/a);
+    cout << ans;
+}
