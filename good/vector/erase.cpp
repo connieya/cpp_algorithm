d804abd6be320397988c6a89d8319022bc4f9f4f
gunny6026 <gunny6026@naver.com>
2022-06-13 00:10:17 +0900

    feat : 프로그래머스 Lv.2 수식 최대화

---
+++ good/vector/erase.cpp
@@ -0,0 +1,33 @@
+﻿#include "bits/stdc++.h"
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    vector<int> v;
+    v.push_back(2);
+    v.push_back(12);
+    v.push_back(5);
+    v.push_back(34);
+    v.push_back(45);
+
+    for (auto s: v) { // 2 , 12 , 5, 34 ,45
+        cout << s << ' ';
+    }
+    cout << '\n';
+
+    v.erase(v.begin());
+    for (auto s: v) { // 12 , 5 , 34, 45
+        cout << s << ' ';
+    }
+    cout << '\n';
+
+    v.erase(v.begin()+2);
+
+    for (auto s: v) { // 12 , 5 , 45
+        cout << s << ' ';
+    }
+    cout << '\n';
+}
