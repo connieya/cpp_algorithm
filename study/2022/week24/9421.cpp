ade673c039ea8c6fd684062e6a6f63771dbfe302
gunny6026 <gunny6026@naver.com>
2022-06-21 19:22:39 +0900

    feat : BOJ 9421 소수상근수

---
+++ study/2022/week24/9421.cpp
@@ -0,0 +1,38 @@
+﻿#include "bits/stdc++.h"
+
+using namespace std;
+
+bool isPrime[1000001];
+int sg[1000001];
+
+bool solve(int num) {
+    if (num == 1) return sg[num] = 1;
+    if (sg[num] != -1) return sg[num];
+    sg[num] = 0;
+    int next = 0, original = num;
+    while (num) {
+        next += (num % 10) * (num % 10);
+        num /= 10;
+    }
+    return sg[original] = solve(next);
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n;
+    cin >> n;
+    memset(sg, -1, sizeof(sg));
+    for (long long i = 2; i <= n; i++) {
+        if (!isPrime[i]) {
+            for (long long j = i * i; j <= n; j += i) {
+                isPrime[j] = true;
+            }
+            if (solve(i)) {
+                cout << i << '\n';
+            }
+        }
+
+    }
+}
---
+++ study/2022/week24/main.cpp
@@ -0,0 +1,17 @@
+﻿#include "bits/stdc++.h"
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int flag[1111];
+    flag[2] = -1;
+    if(flag[2]){
+        cout << "22";
+    }else {
+        cout << "44";
+    }
+}
