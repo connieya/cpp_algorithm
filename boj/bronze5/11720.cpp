ba555987900a3b4e5f11d6256a533c88b2bb9152
gunny6026 <gunny6026@naver.com>
2022-06-24 20:38:53 +0900

    feat : BOJ 11720 숫자의 합

---
+++ boj/bronze5/11720.cpp
@@ -0,0 +1,16 @@
+﻿#include "bits/stdc++.h"
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n;
+    string str;
+    cin >> n >> str;
+    int ans  = 0;
+    for(char ch : str) ans += ch-'0';
+    cout << ans;
+}
