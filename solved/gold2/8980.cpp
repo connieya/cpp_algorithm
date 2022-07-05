4de1672fb2a3bb411f15d55e32140bec54bfd40d
gunny6026 <gunny6026@naver.com>
2022-07-05 23:03:42 +0900

    feat : BOJ 2527 직사각형

---
+++ solved/gold2/8980.cpp
@@ -0,0 +1,34 @@
+﻿#include "bits/stdc++.h"
+
+using namespace std;
+
+struct Info {
+    int start, end, cnt;
+
+    bool operator<(Info i) {
+        if (i.end == end) {
+            return start < i.start;
+        }
+        return end < i.end;
+    }
+
+};
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n, k, m;
+    vector<Info> v;
+    cin >> n >> k >> m;
+    v.resize(m);
+    for (int i = 0; i < m; ++i) {
+        cin >> v[i].start >> v[i].end >> v[i].cnt;
+    }
+    sort(v.begin(), v.end());
+    int ans = 0;
+
+
+
+}
---
+++ study/2022/week25/2564.cpp
@@ -0,0 +1,29 @@
+﻿#include "bits/stdc++.h"
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n, m, t, ans = 0, x, y;
+    cin >> n >> m >> t;
+    vector<pair<int, int>> v;
+    while (t--) {
+        cin >> x >> y;
+        v.push_back({x, y});
+    }
+    cin >> x >> y;
+    for (int i = 0; i < v.size(); ++i) {
+        auto cur = v[i];
+        int a = cur.first;
+        int b = cur.second;
+        if (a == x) {
+            ans += abs(b - y);
+        } else if (x == 1 || x == 2) {
+
+        } else if (x == 3 || x == 4) {
+
+        }
+    }
+}
