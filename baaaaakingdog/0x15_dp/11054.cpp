bfb634f4cdeb78903a4545c2be7c374d33ba24d7
gunny6026 <gunny6026@naver.com>
2022-08-02 05:08:15 +0900

    feat : BOJ 11054 가장 긴 바이토닉 부분 수열

--- baaaaakingdog/0x15_dp/11054.cpp
+++ baaaaakingdog/0x15_dp/11054.cpp
@@ -6,8 +6,7 @@ int arr[1001];
 int dp1[1001];
 int dp2[1001];
 
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
@@ -16,4 +15,27 @@ int main()
     for (int i = 0; i < n; ++i) {
         cin >> arr[i];
     }
+    for (int i = 0; i < n; ++i) {
+        dp1[i] = 1;
+        for (int j = i - 1; j >= 0; --j) {
+            if (arr[i] > arr[j] && dp1[i] < dp1[j] + 1) {
+                dp1[i] = dp1[j] + 1;
+            }
+        }
+    }
+    for (int i = n-1; i >=0; --i) {
+        dp2[i] = 1;
+        for (int j = i+1; j <n ; ++j) {
+            if(arr[i] > arr[j] && dp2[i] < dp2[j]+1) {
+                dp2[i] = dp2[j]+1;
+            }
+        }
+    }
+    int ans = 1;
+    for (int i = 0; i < n; ++i) {
+        if(dp1[i]+dp2[i]-1 > ans){
+            ans = dp1[i]+dp2[i]-1;
+        }
+    }
+    cout << ans;
 }
