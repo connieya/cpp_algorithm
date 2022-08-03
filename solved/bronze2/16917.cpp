d33a73514a9522212e8dc9503bb73e201589dcef
gunny6026 <gunny6026@naver.com>
2022-08-04 08:45:08 +0900

    feat : BOJ 16917 양념 반 후라이드 반

---
+++ solved/bronze2/16917.cpp
@@ -0,0 +1,18 @@
+﻿#include "bits/stdc++.h"
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int a,b,c,x,y;
+    cin >> a >> b >> c >> x >> y;
+    int ans = x*a+b*y;
+    for (int i = 1; i <= 100000; ++i) {
+        int price = 2*i*c + max(0,x-i)*a + max(0,y-i)*b;
+        if(ans > price) ans = price;
+    }
+    cout << ans;
+}
