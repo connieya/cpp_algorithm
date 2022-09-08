3f95d70bb39729ded2840fc1cc94a161b01cdd1e
gunny6026 <gunny6026@naver.com>
2022-09-08 21:46:39 +0900

    feat: BOJ 2606 바이러스

--- README.md
+++ README.md
@@ -59,6 +59,7 @@
 |2531|[ 회전 초밥 ](https://www.acmicpc.net/problem/2531) | 🥈 실버 1 | 투 포인터~ |[코드](boj/silver1/2531.md)
 |2573|[ 빙산 ](https://www.acmicpc.net/problem/2573) | 🥇 골드 4 | DFS & BFS 응용 👍👍👍 |[코드](boj/gold4/2573.md)
 |2602|[ 돌다리 건너기 ](https://www.acmicpc.net/problem/2602) |🥇 골드 4 |  0 과 1 toggle & DP |[코드](boj/gold4/2602.md)
+|2606|[ 바이러스 ](https://www.acmicpc.net/problem/2602) |🥈 실버 3 | DFS/BFS , 유니온 파인드 |[코드](boj/silver3/2606.md)
 |2617|[ 구슬 찾기 ](https://www.acmicpc.net/problem/2617) |🥇 골드 5 | 플로이드 와샬  |[코드](jungle/week3/2617_1.cpp)
 |2623|[ 음악프로그램 ](https://www.acmicpc.net/problem/2623) |🥇 골드 2 | 위상정렬  |[코드](boj/gold2/2623.md)
 |2630|[ 색종이 만들기 ](https://www.acmicpc.net/problem/2630) |🥈 실버 2 | 분할정복  |[코드](boj/silver2/2630.md)
--- jungle/week3/2606.cpp
+++ jungle/week3/2606.cpp
@@ -1,36 +1,39 @@
 ﻿#include "bits/stdc++.h"
 
 using namespace std;
-vector<int> graph[101];
-bool checked[101];
 
-void dfs(int start, int prev) {
-    checked[start] = true;
-    cout << start << '\n';
-    for (int nxt: graph[start]) {
-        if (prev == nxt)continue;
-        dfs(nxt, start);
-    }
+int parent[101];
+
+int Find(int x){
+    if(x == parent[x]) return  x;
+    return parent[x] = Find(parent[x]);
+}
+
+void Union(int x, int y){
+    x = Find(x);
+    y = Find(y);
+
+    if(x > y) swap(x,y);
+    parent[y] = x;
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    int n, e;
-    cin >> n >> e;
-    for (int i = 0; i < e; ++i) {
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i <= n; ++i) {
+        parent[i] = i;
+    }
+    for (int i = 0; i < m; ++i) {
         int a, b;
         cin >> a >> b;
-        graph[a].push_back(b);
-        graph[b].push_back(a);
+        Union(a,b);
     }
-    dfs(1, 1);
     int ans = 0;
     for (int i = 2; i <= n; ++i) {
-        ans += checked[i];
-
+        ans += (Find(i) == 1);
     }
     cout << ans;
-
 }
