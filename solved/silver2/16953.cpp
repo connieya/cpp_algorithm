02c32c807b655fc608bf09e478d5f6092a74d0c2
gunny6026 <gunny6026@naver.com>
2022-09-11 12:44:00 +0900

    feat: BOJ 16953 A->B

---
+++ solved/silver2/16953.cpp
@@ -0,0 +1,26 @@
+﻿#include "bits/stdc++.h"
+
+using namespace std;
+typedef long long ll;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll a, b;
+    cin >> a >> b;
+    for (int i = 1; a <= b; ++i) {
+        if (a == b) {
+            cout << i;
+            return 0;
+        }
+        if (b % 2 == 0) {
+            b /= 2;
+        } else if (b % 10 == 1) {
+            b = (b - 1) / 10;
+        } else {
+            break;
+        }
+    }
+    cout << -1;
+}
