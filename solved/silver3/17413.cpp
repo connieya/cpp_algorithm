6661910294d845191397a52b1ce9a4cff0ebc4fe
gunny6026 <gunny6026@naver.com>
2022-09-23 20:49:02 +0900

    feat: BOJ 17413 단어 뒤집기 2

---
+++ solved/silver3/17413.cpp
@@ -0,0 +1,33 @@
+﻿#include "bits/stdc++.h"
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    string str;
+    getline(cin, str);
+    string tmp;
+    int n = str.size();
+    for (int i = 0; i < n;) {
+        if (str[i] == ' '){
+            i++;
+            continue;
+        }
+        if (str[i] == '<') {
+            int j = i;
+            while (j < n && str[j] != '>') {
+                j++;
+            }
+            i = j + 1;
+        }
+        int j = i;
+        while (j < n && str[j] != ' ' && str[j] != '<') {
+            j++;
+        }
+        reverse(str.begin() + i, str.begin() + j);
+        i = j;
+    }
+    cout << str;
+}
