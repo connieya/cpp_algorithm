ffe11af54c03526a15af01a5cbb20e240ef02d54
gunny6026 <gunny6026@naver.com>
2022-11-07 13:34:05 +0900

    feat: BOJ 1110 더하기 사이클

---
+++ solved/bronze1/1110.cpp
@@ -0,0 +1,18 @@
+﻿#include "bits/stdc++.h"
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n;
+    cin >> n;
+    int answer = 0;
+    int target = n;
+    do {
+        target = (target % 10) * 10 + ((target / 10 + target % 10)) % 10;
+        answer++;
+    } while (target != n);
+    cout << answer;
+}
